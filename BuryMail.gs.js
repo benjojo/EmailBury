d1cb652fe09441bd1e58e6224cc6a959f4c3db1e
Ben Cox <ben@benjojo.co.uk>
2014-05-03 11:51:05 +0100

    Untested way of generating the searches for gmail

--- BuryMail.gs.js
+++ BuryMail.gs.js
@@ -1,7 +1,10 @@
 function BuryOldEmail() {
   var BuryTag = "<insert label here>";
   var ONE_WEEK = 60 * 60 * 24 * 7 * 1000;
-  var threads = GmailApp.search("label:"+BuryTag);
+  var ONE_DAY = 60 * 60 * 24 * 1000;
+  var now = new Date();
+  var expire = new Date(new Date() - (ONE_WEEK + ONE_DAY));
+  var threads = GmailApp.search("after:"+expire.getFullYear()+"/"+(expire.getMonth()+1)+"/"+expire.getDate()+" before:"+now.getFullYear()+"/"+(now.getMonth()+1)+"/"+now.getDate()+" label:"+BuryTag);
   for (var i = 0; i < threads.length; i++) {
 
     if(((new Date) - threads[i].getLastMessageDate()) > ONE_WEEK) {
