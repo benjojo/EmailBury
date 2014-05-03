2ad2c67411fba7ea337e140c44682bd6663cbd9a
Ben Cox <ben@benjojo.co.uk>
2014-05-03 11:41:24 +0100

    First commit, Based off my Decay script

---
+++ BuryMail.gs.js
@@ -0,0 +1,12 @@
+function BuryOldEmail() {
+  var BuryTag = "<insert label here>";
+  var ONE_WEEK = 60 * 60 * 24 * 7 * 1000;
+  var threads = GmailApp.search("label:"+BuryTag);
+  for (var i = 0; i < threads.length; i++) {
+
+    if(((new Date) - threads[i].getLastMessageDate()) > ONE_WEEK) {
+      threads[i].markRead();
+    }
+
+  }
+}
